9df3b8b4f0de4f4b6640421dc52a6ecb90ae6c83
Foge65 <mr.texnik666@gmail.com>
2023-12-14 04:05:39 +0300

    rename package

--- SpringWebServer/src/main/java/team/firestorm/auditcalculatorserver/SpringMain.java
+++ SpringWebServer/src/main/java/team/firestorm/auditcalculatorserver/SpringMain.java
@@ -1,4 +1,4 @@
-package starter;
+package team.firestorm.auditcalculatorserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
