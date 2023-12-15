1f9767884d9a2a1d9b856bf24930df9f020e1833
Foge65 <mr.texnik666@gmail.com>
2023-12-15 19:28:50 +0300

    change method number version from ram to file

--- SpringWebServer/src/main/java/team/firestorm/auditcalculatorserver/VersionController.java
+++ SpringWebServer/src/main/java/team/firestorm/auditcalculatorserver/VersionController.java
@@ -7,22 +7,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 @RestController
 public class VersionController {
-    private static final String CURRENT_VERSION = "1.0";
 
     @GetMapping("/version")
+    @SneakyThrows
     public String currentVersion() {
-        return CURRENT_VERSION;
+        Path filePath = Paths.get("", "version").toAbsolutePath();
+        List<String> lines = Files.readAllLines(filePath);
+        return String.join("", lines);
     }
 
     @GetMapping("/download")
     @SneakyThrows
     public ResponseEntity<Resource> update() {
-            Resource resource = new UrlResource(Paths.get("/home/foge/java/AuditCalculator/app.exe").toUri());
-            return ResponseEntity.ok()
-                    .body(resource);
+        Resource resource = new UrlResource(Paths.get("/home/foge/java/AuditCalculator/app.exe").toUri());
+        return ResponseEntity.ok()
+                .body(resource);
     }
 }
