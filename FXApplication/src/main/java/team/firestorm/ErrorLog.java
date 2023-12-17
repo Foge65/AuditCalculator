fe5becc0d02b81fd3132563a4d4f167fbb81655c
Foge65 <mr.texnik666@gmail.com>
2023-12-18 00:33:10 +0300

    [PS1.4] date in Errors file

--- FXApplication/src/main/java/team/firestorm/ErrorLog.java
+++ FXApplication/src/main/java/team/firestorm/ErrorLog.java
@@ -2,12 +2,16 @@ package team.firestorm;
 
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ErrorLog {
     public static void errorsToFile() {
         Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
-            try (PrintWriter printWriter = new PrintWriter(new FileWriter("errors.log", true))) {
-                printWriter.println("=== Uncaught Exception ===");
+            try (PrintWriter printWriter = new PrintWriter(new FileWriter("Errors.log", true))) {
+                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+                LocalDateTime localDateTime = LocalDateTime.now();
+                printWriter.println("=== Uncaught Exception === " + dateTimeFormatter.format(localDateTime));
                 throwable.printStackTrace(printWriter);
             } catch (Exception e) {
                 e.printStackTrace();
