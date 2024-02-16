f763a90711f45f5b85381406137ef4b6eebbb036
Foge65 <mr.texnik666@gmail.com>
2024-02-16 17:08:22 +0300

    [Spring] test for setNewVersion

--- SpringWebServer/src/test/java/team/firestorm/auditcalculatorserver/VersionControllerTest.java
+++ SpringWebServer/src/test/java/team/firestorm/auditcalculatorserver/VersionControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,5 +22,15 @@ class VersionControllerTest {
         Pattern pattern = Pattern.compile("^\\d*+");
         Matcher matcher = pattern.matcher(currentVersion);
         Assertions.assertTrue(matcher.find());
+    }
+
+    @Test
+    void setNewVersion() {
+        String initVersion = controller.currentVersion();
+        String currentVersion = controller.currentVersion();
+        controller.setNewVersion(Map.of("version", "999"));
+        String newVersion = controller.currentVersion();
+        Assertions.assertNotEquals(currentVersion, newVersion);
+        controller.setNewVersion(Map.of("version", initVersion));
     }
 }
