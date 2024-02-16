55167f5215f98b19cc4894a5fb2ddb12f6d228cd
Foge65 <mr.texnik666@gmail.com>
2024-02-16 17:25:24 +0300

    reformat

--- FXApplication/src/main/java/team/firestorm/pokerstars/model/ElementIndex.java
+++ FXApplication/src/main/java/team/firestorm/pokerstars/model/ElementIndex.java
@@ -2,9 +2,14 @@ package team.firestorm.pokerstars.model;
 
 public interface ElementIndex {
     int getAmountIndex();
+
     int getStarsCoinAmountIndex();
+
     int getTMoneyAmountIndex();
+
     int getBalanceIndex();
+
     int getCoinBalanceIndex();
+
     int getTMoneyBalanceIndex();
 }
